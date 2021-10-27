1f171f07ec41989852db511709346475ac93365b
Brent Bowles <bbowles@sileotech.com>
2021-10-27 16:53:32 -0700

    create new

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 //route that the front-end can request data from
 const { animals } = require('./data/animals');
-const fs = require('fs');
-const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 //instantiate the server and tell it to listen for requests
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
 //we're breaking the Query selector into its own function. This will keep our code maintainable and clean.
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -59,39 +62,6 @@ function filterByQuery(query, animalsArray) {
     // return finished code to post route for response
     return animal;
   }
-//get method: first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
-  //specific animals 
-  app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {  
-    res.json(result);
-    } else {
-        res.send(404);
-    }
-  });
-//defined a route that listens for POST requests
-  app.post('/api/animals', (req, res) => {
-      req.body.id = animals.length.toString();
-
-//validation if data is incorrect in req.body, send back 400 error
-if (!validateAnimal(req.body)){
-    res.status(400).send('the animal is not properly formatted.');
-}else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-}
-});
-//method to make the server listen
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
 
   //validation for new animals
   function validateAnimal(animal) {
@@ -109,3 +79,39 @@ app.listen(PORT, () => {
     }
     return true;
   }
+
+//get method: first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+  //specific animals 
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {  
+    res.json(result);
+    } else {
+        res.send(404);
+    }
+  });
+  
+//defined a route that listens for POST requests
+  app.post('/api/animals', (req, res) => {
+      req.body.id = animals.length.toString();
+
+//validation if data is incorrect in req.body, send back 400 error
+if (!validateAnimal(req.body)){
+    res.status(400).send('the animal is not properly formatted.');
+}else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+}
+});
+//method to make the server listen
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
+
