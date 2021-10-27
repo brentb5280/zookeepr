9d2a93dcf2ecaea2e9cb2407d5c204eb33528fe0
Brent Bowles <bbowles@sileotech.com>
2021-10-27 16:29:31 -0700

    add new animal

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+const express = require('express');
 //route that the front-end can request data from
 const { animals } = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
 
-const express = require('express');
 const PORT = process.env.PORT || 3001;
 //instantiate the server and tell it to listen for requests
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 //we're breaking the Query selector into its own function. This will keep our code maintainable and clean.
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -20,13 +26,6 @@ function filterByQuery(query, animalsArray) {
       }
       // Loop through each trait in the personalityTraits array:
       personalityTraitsArray.forEach(trait => {
-        // Check the trait against each animal in the filteredResults array.
-        // Remember, it is initially a copy of the animalsArray,
-        // but here we're updating it for each trait in the .forEach() loop.
-        // For each trait being targeted by the filter, the filteredResults
-        // array will then contain only the entries that contain the trait,
-        // so at the end we'll have an array of animals that have every one 
-        // of the traits when the .forEach() loop is finished.
         filteredResults = filteredResults.filter(
           animal => animal.personalityTraits.indexOf(trait) !== -1
         );
@@ -49,6 +48,17 @@ function filterByQuery(query, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
   }
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(_dirnames, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+  
+    // return finished code to post route for response
+    return animal;
+  }
 //get method: first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -66,7 +76,36 @@ app.get('/api/animals', (req, res) => {
         res.send(404);
     }
   });
+//defined a route that listens for POST requests
+  app.post('/api/animals', (req, res) => {
+      req.body.id = animals.length.toString();
+
+//validation if data is incorrect in req.body, send back 400 error
+if (!validateAnimal(req.body)){
+    res.status(400).send('the animal is not properly formatted.');
+}else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+}
+});
 //method to make the server listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
+  //validation for new animals
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
